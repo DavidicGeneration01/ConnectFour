2c7995a5ba30fb084ab71fb871a1c35636dbeb48
Alade David <alade.david@email.com>
2026-09-24 22:44:56 +0100

    Add ConnectFour project files and tests

---
+++ ConnectFour.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using ConnectFour;
+using Xunit;
+
+namespace ConnectFour.Tests;
+
+public class GameStateWinTests
+{
+    [Fact]
+    public void PlayerOneWins_WhenFourConnectedHorizontally()
+    {
+        var game = new GameState();
+
+        // Player 1 horizontal win: columns 0,1,2,3 on bottom row
+        game.TheBoard[35] = 1;
+        game.TheBoard[36] = 1;
+        game.TheBoard[37] = 1;
+        game.TheBoard[38] = 1;
+
+        var result = game.CheckForWin();
+
+        Assert.Equal(GameState.WinState.Player1_Wins, result);
+    }
+}
